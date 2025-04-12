dc857e209782ebd574862d4d59f566ae8e1ff3e9
Alex Wu <poorwym@srv790030.hstgr.cloud>
2025-04-12 17:36:53 +0000

    finish ep1

---
+++ scripts/Build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=`realpath Walnut/Walnut-Modules/Walnut-Networking/vendor/GameNetworkingSockets/bin/Linux`
+
+make -j8
---
+++ scripts/Run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=`realpath Walnut/Walnut-Modules/Walnut-Networking/vendor/GameNetworkingSockets/bin/Linux`
+
+exec bin/Debug-linux-x86_64/Cubed-Server/Cubed-Server
